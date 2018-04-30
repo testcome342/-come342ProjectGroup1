6645cf907a75f0d4ac28f294224f1f8a669451a8
arbade <arbade@gmail.com>
2018-04-30 23:48:12 +0300

    -ScanHelper has created

---
+++ AgateLtd/src/helper/ScanHelper.java
@@ -0,0 +1,27 @@
+package helper;
+
+import java.util.Scanner;
+
+public class ScanHelper {
+	
+	//Arda Batuhan Demir
+	//30.04.2018
+	
+
+	private static Scanner scanner;
+
+	public static int scanInt() {
+		scanner = new Scanner(System.in);
+		return scanner.nextInt();
+	}
+	
+	public static String scanString() {
+		scanner = new Scanner(System.in);
+		return scanner.nextLine();
+	}
+	
+	public static Double scanDouble() {
+		scanner = new Scanner(System.in);
+		return scanner.nextDouble();
+	}
+}
