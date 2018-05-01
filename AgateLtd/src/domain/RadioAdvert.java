7faac0b9022ea0f872b135b5cc5a347108b3b99c
arbade <arbade@gmail.com>
2018-05-01 20:39:09 +0300

    -RadioAdvert and TvAdvert has created

---
+++ AgateLtd/src/domain/RadioAdvert.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class RadioAdvert extends DigitalAdvert {
+
+	public RadioAdvert(String advertTitle, String targetCompletionDate, Double estimatedAdvertCost,
+			Double actualAdvertCost) {
+		super(advertTitle, targetCompletionDate, estimatedAdvertCost, actualAdvertCost);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ AgateLtd/src/domain/TvAdvert.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class TvAdvert extends DigitalAdvert{
+	
+	/*
+	 * 
+	 * Arda Batuhan Demir 
+	 * 01.05.2018
+	 */
+
+	public TvAdvert(String advertTitle, String targetCompletionDate, Double estimatedAdvertCost,
+			Double actualAdvertCost) {
+		super(advertTitle, targetCompletionDate, estimatedAdvertCost, actualAdvertCost);
+		
+	}
+
+}
